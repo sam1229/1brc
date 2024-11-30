86bd58454d37fe949d9c80f3de57cbbdc1b7c9f5
Sameeran A <sameeran29@gmail.com>
2024-11-30 22:59:51 +0530

    made it single threaded again - huge performance gain :P - and added custom temperature parsing to int - small performance gain.

--- src/main/java/org/sameeran/Main.java
+++ src/main/java/org/sameeran/Main.java
@@ -6,20 +6,19 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import static java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class Main {
 
     private static class Stats {
-        float min;
-        double avg;
-        float max;
+        int min;
+        int avg;
+        int max;
         int count;
-        Stats(float min, double avg, float max, int count) {
+        Stats(int min, int avg, int max, int count) {
             this.min = min;
             this.max = max;
             this.avg = avg;
@@ -30,7 +29,7 @@ public class Main {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
-        var map = new ConcurrentHashMap<String, Stats>();
+        var map = new HashMap<String, Stats>();
 
         BiFunction<Stats,Stats,Stats> statsFn = (x,y) -> {
             x.min = Math.min(x.min, y.min);
@@ -41,28 +40,25 @@ public class Main {
         };
 
         int nrec = 0;
-        var pool = Executors.newWorkStealingPool();
         try(var reader = new BufferedReader(new FileReader("src/main/resources/measurements_100M.txt"))) {
 
             for (var line = reader.readLine(); line != null; line = reader.readLine()) {
                 String finalLine = line;
-                pool.submit(() -> update_stat_map(finalLine, map, statsFn));
+                update_stat_map(finalLine, map, statsFn);
             }
         } catch (Exception e) {
             System.out.println(Arrays.stream(e.getStackTrace()).sequential());
         }
 
-        pool.close();
-
         var mid = System.currentTimeMillis();
 
         var ans = map.entrySet().stream()
             .map(e -> new SimpleImmutableEntry<>(e.getKey(),
-                    BigDecimal.valueOf(e.getValue().min).setScale(1, RoundingMode.HALF_UP).toPlainString()
+                    BigDecimal.valueOf((float)e.getValue().min/10.0).setScale(1, RoundingMode.HALF_UP).toPlainString()
                             .concat("/")
-                            .concat(BigDecimal.valueOf(e.getValue().max).setScale(1, RoundingMode.HALF_UP).toPlainString())
+                            .concat(BigDecimal.valueOf((float)e.getValue().max/10).setScale(1, RoundingMode.HALF_UP).toPlainString())
                             .concat("/")
-                            .concat(BigDecimal.valueOf(e.getValue().avg/e.getValue().count)
+                            .concat(BigDecimal.valueOf((float)e.getValue().avg/(10.0*e.getValue().count))
                                     .setScale(1, RoundingMode.HALF_UP).toPlainString())))
                 .sorted(Map.Entry.comparingByKey())
                 .collect(Collectors.toList());
@@ -73,13 +69,20 @@ public class Main {
         System.out.println(ans);
     }
 
-    private static void update_stat_map(String line,
-                                        ConcurrentHashMap<String, Stats> map, BiFunction<Stats, Stats, Stats> fn) {
+    private static void update_stat_map(String line, Map<String, Stats> map, BiFunction<Stats, Stats, Stats> fn) {
         var lineArr = splitString(line);
         var k = lineArr[0];
-        var v = Float.parseFloat(lineArr[1]);
+        var v = parseTemp(lineArr[1]);
 
         map.merge(k, new Stats(v,v,v,1), fn);
+    }
+
+    // this (using int instead of float) adds a small performance gain over using floats
+    private static int parseTemp(String num) {
+        num = num.replace(".","");
+        // can't see a significant difference for writing a custom fuction to replace
+        // might explore later
+        return Integer.parseInt(num);
     }
 
     private static String[] splitString(String str) {
